d760fb6143183109faa2a132887973685345e4c1
ujagare <ujagare@gmail.com>
2025-05-14 13:05:40 +0530

    Fix App import paths in main.js and main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import App from "App.jsx";
+import App from "./App.jsx";
 import "./index.css";
 
 // Create root and render app
